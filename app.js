deccf7920d5d123faf4d48e6334e997f6d740a73
Manoel Nascimento <mathenascimento76@gmail.com>
2025-12-26 16:37:06 -0300

    Alteração na branch dev

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello Geeks!');
+    res.send('Hello Wordl - branch dev!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
